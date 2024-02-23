a55c2137213b54775a3ded39f2bc5a0fb34d2b25
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-23 22:24:26 +0100

    Run spring app for a first time and try to get name of Speaker

---
+++ src/main/java/io/github/bartlomiejmilosz/conferenceapp/Application.java
@@ -0,0 +1,14 @@
+package io.github.bartlomiejmilosz.conferenceapp;
+
+import io.github.bartlomiejmilosz.conferenceapp.service.SpeakerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(
+                AppConfig.class);
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service.findAll().get(0).getFirstName());
+    }
+}
