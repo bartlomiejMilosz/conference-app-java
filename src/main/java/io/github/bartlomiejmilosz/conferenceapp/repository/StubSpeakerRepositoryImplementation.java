7479d8f3ec7b5ff1605f35c879cae74eebd9f290
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-24 09:28:03 +0100

    Mark stereotypes on repository and service and add @Autowired to complete spring configuration stage

--- src/main/java/io/github/bartlomiejmilosz/conferenceapp/repository/StubSpeakerRepositoryImplementation.java
+++ src/main/java/io/github/bartlomiejmilosz/conferenceapp/repository/StubSpeakerRepositoryImplementation.java
@@ -1,10 +1,11 @@
 package io.github.bartlomiejmilosz.conferenceapp.repository;
 
 import io.github.bartlomiejmilosz.conferenceapp.model.Speaker;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Repository("speakerRepository")
 public class StubSpeakerRepositoryImplementation implements SpeakerRepository {
     @Override
     public List<Speaker> findAll() {
--- src/main/java/io/github/bartlomiejmilosz/conferenceapp/service/SpeakerServiceImplementation.java
+++ src/main/java/io/github/bartlomiejmilosz/conferenceapp/service/SpeakerServiceImplementation.java
@@ -2,13 +2,25 @@ package io.github.bartlomiejmilosz.conferenceapp.service;
 
 import io.github.bartlomiejmilosz.conferenceapp.model.Speaker;
 import io.github.bartlomiejmilosz.conferenceapp.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
+@Scope(value = BeanDefinition.SCOPE_SINGLETON)
 public class SpeakerServiceImplementation implements SpeakerService {
     private SpeakerRepository speakerRepository;
 
+    public SpeakerServiceImplementation() {
+        System.out.println("SpeakerServiceImplementation no args constructor");
+    }
+
+    @Autowired
     public SpeakerServiceImplementation(SpeakerRepository speakerRepository) {
+        System.out.println("SpeakerServiceImplementation repository constructor");
         this.speakerRepository = speakerRepository;
     }
 
@@ -17,7 +29,9 @@ public class SpeakerServiceImplementation implements SpeakerService {
         return speakerRepository.findAll();
     }
 
+    //@Autowired
     public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        System.out.println("SpeakerServiceImplementation setter");
         this.speakerRepository = speakerRepository;
     }
 }
