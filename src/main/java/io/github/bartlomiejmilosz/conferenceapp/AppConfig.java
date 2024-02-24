77f7409a1feef5c1f5d8a879631250c7e63062c5
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-24 09:32:17 +0100

    Cleanup unused comments in AppConfig

--- src/main/java/io/github/bartlomiejmilosz/conferenceapp/AppConfig.java
+++ src/main/java/io/github/bartlomiejmilosz/conferenceapp/AppConfig.java
@@ -1,29 +1,10 @@
 package io.github.bartlomiejmilosz.conferenceapp;
 
-import io.github.bartlomiejmilosz.conferenceapp.repository.SpeakerRepository;
-import io.github.bartlomiejmilosz.conferenceapp.repository.StubSpeakerRepositoryImplementation;
-import io.github.bartlomiejmilosz.conferenceapp.service.SpeakerService;
-import io.github.bartlomiejmilosz.conferenceapp.service.SpeakerServiceImplementation;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 @ComponentScan({"io.github.bartlomiejmilosz"})
 public class AppConfig {
-   /* @Bean(name = "speakerService")
-    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
-    public SpeakerService getSpeakerService() {
-        //var service = new SpeakerServiceImplementation(getSpeakerRepository());
-        var service = new SpeakerServiceImplementation();
-        //service.setSpeakerRepository(getSpeakerRepository());
-        return service;
-    }*/
 
-    /*@Bean(name = "speakerRepository")
-    public SpeakerRepository getSpeakerRepository() {
-        return new StubSpeakerRepositoryImplementation();
-    }*/
 }
