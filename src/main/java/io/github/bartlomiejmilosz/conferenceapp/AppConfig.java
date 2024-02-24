a3bacfddd15224574e0761265873529a8d8150e5
Bart Milo <bartlomiej.milosz@gmail.com>
2024-02-24 09:25:51 +0100

    Add component scanning in configuration file

--- src/main/java/io/github/bartlomiejmilosz/conferenceapp/AppConfig.java
+++ src/main/java/io/github/bartlomiejmilosz/conferenceapp/AppConfig.java
@@ -6,21 +6,24 @@ import io.github.bartlomiejmilosz.conferenceapp.service.SpeakerService;
 import io.github.bartlomiejmilosz.conferenceapp.service.SpeakerServiceImplementation;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@ComponentScan({"io.github.bartlomiejmilosz"})
 public class AppConfig {
-    @Bean(name = "speakerService")
+   /* @Bean(name = "speakerService")
     @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
-        var service = new SpeakerServiceImplementation(getSpeakerRepository());
+        //var service = new SpeakerServiceImplementation(getSpeakerRepository());
+        var service = new SpeakerServiceImplementation();
         //service.setSpeakerRepository(getSpeakerRepository());
         return service;
-    }
+    }*/
 
-    @Bean(name = "speakerRepository")
+    /*@Bean(name = "speakerRepository")
     public SpeakerRepository getSpeakerRepository() {
         return new StubSpeakerRepositoryImplementation();
-    }
+    }*/
 }
